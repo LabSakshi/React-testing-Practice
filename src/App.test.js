5ef8359062b1a76ecaf8bc2b07c69078e2ecd2f7
Sakshi Gupta <sakshigupta0513@gmail.com>
2022-02-11 14:39:15 +0530

    React test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('to check if there is a component with button role', () => {
+  //select the component that needs to be tested
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+  //get the element out of the component
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' })
+
+  //write out what you expect from that test i.e. we expect the button to have bgc of red
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+
+  //click button fireEvent will click the button
+  fireEvent.click(colorButton);
+
+  //After click expect the button to have background color to blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' })
+
+  //expect the button text  to be 'changes to red'
+  expect(colorButton.textContent).toBe('Change to red')
+})
+
+
+test('initial conditions', () => {
+  render(<App />);
+
+  //check that the button starts out enabled
+
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' })
+})
+
+// test('button has correct initial text', () => {
+
+// });
+
+// test('button turns blue when clicked', () => {
+
+// })
+
