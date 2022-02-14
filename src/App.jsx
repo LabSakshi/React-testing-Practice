fca40e1b4d5a03020dae2430e2029411a20b6bba
Sakshi Gupta <sakshigupta0513@gmail.com>
2022-02-14 19:23:25 +0530

    function unit test

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
 import { useState } from 'react';
-
+export function replaceCamelWithSpaces(colorName) {
+  return colorName.split(/(?=[A-Z])/).join(' ')
+}
 function App() {
-  const [ buttonColor, setButtonColor ] = useState('red');
-  const [ disabled, setdisabled ] = useState(false);
-  
-  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
-  
+  const [buttonColor, setButtonColor] = useState('MediumVioletRed');
+  const [disabled, setdisabled] = useState(false);
+
+  const newButtonColor = buttonColor === 'MediumVioletRed' ? 'MidnightBlue' : 'MediumVioletRed';
+
+
   return (
     <div>
-        <button
-        style={{backgroundColor: disabled?'gray':buttonColor}}
+      <button
+        style={{ backgroundColor: disabled ? 'gray' : buttonColor }}
         onClick={() => setButtonColor(newButtonColor)}
         disabled={disabled}
-      >Change to {newButtonColor}</button>
-    <br />
-    <input
+      >Change to {replaceCamelWithSpaces(newButtonColor)}</button>
+      <br />
+      <input
         type="checkbox"
         id="disable-button-checkbox"
         defaultChecked={disabled}
         aria-checked={disabled}
         onChange={(e) => setdisabled(e.target.checked)} />
-        <label htmlFor="disable-button-checkbox">Disable button</label>
-    </div>  
+      <label htmlFor="disable-button-checkbox">Disable button</label>
+    </div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import App, { replaceCamelWithSpaces } from './App';
 
 test('Confirm button is disabled when checkbox is checked', () => {
 
@@ -9,16 +9,34 @@ test('Confirm button is disabled when checkbox is checked', () => {
   const button = screen.getByRole('button');
   expect(button).toBeEnabled()
   //select the checkbox and check your expectations initially from the checkbox
-  const checkbox = screen.getByRole('checkbox',{name:'Disable button'})
+  const checkbox = screen.getByRole('checkbox', { name: 'Disable button' })
   expect(checkbox).not.toBeChecked()
 
-//Click the checkbox
+  //Click the checkbox
 
-//TEST FLOW disable button-->button is gray-->enable button-->button is gray
+  //TEST FLOW disable button-->button is gray-->enable button-->button is gray
   fireEvent.click(checkbox)
   expect(button).toBeDisabled()
-  expect(button).toHaveStyle({backgroundColor:'gray'})
+  expect(button).toHaveStyle({ backgroundColor: 'gray' })
   fireEvent.click(checkbox)
   expect(button).toBeEnabled()
-  expect(button).toHaveStyle({backgroundColor:'red'})
+  expect(button).toHaveStyle({ backgroundColor: 'MediumVioletRed' })
+})
+
+// FUNCTIONAL TESTING: DESCRIBE TAKES IN TEST GLOBAL
+
+describe('spaced before camel case capital letters', () => {
+  test('Work for no inner capital letters', () => {
+    expect(replaceCamelWithSpaces('Red')).toBe('Red')
+  });
+
+  test('Works for one inner capital letter', () => {
+    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue')
+  });
+
+  test('Works for multiple innrercapital letters', () => {
+    expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red')
+  })
+
+
 })
